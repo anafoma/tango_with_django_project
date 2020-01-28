c936df4008ff740605eb40021dbc6907ba9960e5
Ana Maria Forero Manzano <2402826f@student.gla.ac.uk>
2020-01-28 10:40:07 +0000

    Chapter 3  Done

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 #Each view exists as a series of individual functions, in this instance called index
 def index(request):
     #Importing the HttpResponse object from the django.http module
-    return HttpResponse("It workssss!")
+   
+    return HttpResponse('Hey there partner <a href="/rango/about/">About</a>')
+
+#view method called about 
+def about(request):
+
+    return HttpResponse('Rango says here is about the page <a href="/rango/">About</a>')
