0493ca7343c936ebed3dfb9a64ca25e1de6661c2
Ana Maria Forero Manzano <2402826f@student.gla.ac.uk>
2020-02-08 17:56:18 +0000

    Chapter 6 Done

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,13 @@
+
 from django.urls import path
-from rango import views 
+from rango import views
 
-app_name='rango'
+app_name = 'rango'
 
-urlpatterns=[
+urlpatterns = [
+
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('index/', views.index, name='index')
-    ]
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+
+]
